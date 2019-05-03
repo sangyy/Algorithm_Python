e17b31ff9693b17b2a1d8f57b5305b65bf376428
sangyy <bj109maker@163.com>
2019-05-03 16:33:51 +0800

    Update Hanoi.py
    哦哦看看

--- Hanoi.py
+++ Hanoi.py
@@ -13,8 +13,11 @@ def hanoi(a,b,c,n):
         count += 1
         print('第',count,'次移动:',a,'->',c)
     else:
+        #把A柱上n-1个圆盘借助C柱移动到B柱上
         hanoi(a,c,b,n-1)
+        #把A柱上最大的圆盘移动到C柱上
         hanoi(a,b,c,1)
+        #把B柱上n-1个圆盘借助A柱移动到C柱上
         hanoi(b,a,c,n-1)
 num = int(input('请输入汉诺塔的圆盘数量:'))    
 hanoi('a','b','c',num)
